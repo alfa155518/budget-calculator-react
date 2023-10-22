53ca9f6c8919b3b526472020ccda44e0a4da556b
Ahmed Hassop <alfaelkanas@gmail.com>
2023-10-22 18:19:52 +0300

    Handel All Fils

--- src/App.js
+++ src/App.js
@@ -3,98 +3,128 @@ import Alert from "./components/Alert";
 import Forme from "./components/ExpenseForm";
 import List from "./components/ExpenseList";
 import { v4 as uuidv4 } from "uuid";
-import "./App.css"
+import "./App.css";
 
-// const initialExpense = [
-//   {id: uuidv4(), charge:"car-1", amount:100},
-//   {id: uuidv4(), charge:"car-2", amount:10},
-//   {id: uuidv4(), charge:"car-3", amount:10}
-// ]
-
-let initialExpense = localStorage.getItem("expenses") ? JSON.parse(localStorage.getItem("expenses")) : [];
+// initial Value
+let initialExpense = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 
 export default function App() {
+  // Set Expenses
+  const [expenses, setExpenses] = useState(initialExpense);
+  // Set Charge
+  const [charge, setCharge] = useState("");
+  // Set Amount
+  const [amount, setAmount] = useState("");
+  // Set Alert
+  const [alert, setAlert] = useState({ show: false });
+  // Set Edit
+  const [edit, setEdit] = useState(false);
+  // Set Id
+  const [id, setId] = useState(0);
 
-  const [expenses, setExpenses] = useState(initialExpense)
-  const [charge, setCharge] = useState('')
-  const [amount, setAmount] = useState('')
-  const [alert, setAlert] = useState({show:false})
-  const [edit, setEdit] = useState(false)
-  const [id, setId] = useState(0)
-
+  // Save Item In Local Storage
   useEffect(() => {
-    localStorage.setItem("expenses", JSON.stringify(expenses))
-  }, [expenses])
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
+  // *********** Function Charge **************** //
   function handelCharge(e) {
-      setCharge(e.target.value)
-  }
-  function handelAmount(e) {
-      setAmount(e.target.value)
+    setCharge(e.target.value);
   }
 
-  function handelAlert({type,text}) {
-    setAlert({show:true,type,text})
+  // *********** Function Amount **************** //
+  function handelAmount(e) {
+    setAmount(e.target.value);
+  }
+
+  // *********** Function Alert **************** //
+  function handelAlert({ type, text }) {
+    setAlert({ show: true, type, text });
     setTimeout(() => {
-      setAlert({show:false})
+      setAlert({ show: false });
     }, 1000);
   }
-  function handelSubmit(e) {
-    e.preventDefault()
-    if(charge !== '' && amount > 0) {
 
+  // *********** Function Submit **************** //
+  function handelSubmit(e) {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      // Edited Item
       if (edit) {
-        let tempExpenses = expenses.map(item =>  {
-          return item.id === id ? {...item, charge, amount} : item;
-        })
-        setExpenses(tempExpenses)
-        setEdit(false)
-        handelAlert({type:"success",text:"Item Edited"})
-      } else {
-        const singleExpense = {id:uuidv4(), charge:charge, amount:amount}
-        setExpenses([...expenses, singleExpense])
-        handelAlert({type:"success",text:"Item Added"})
+        let tempExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handelAlert({ type: "success", text: "Item Edited" });
       }
-      setCharge('')
-      setAmount('')
+
+      // Add Item
+      else {
+        const singleExpense = { id: uuidv4(), charge: charge, amount: amount };
+        setExpenses([...expenses, singleExpense]);
+        handelAlert({ type: "success", text: "Item Added" });
+      }
+      setCharge("");
+      setAmount("");
     } else {
-      handelAlert({type:"danger",text:"Item Not Added"})
+      handelAlert({ type: "danger", text: "Item Not Added" });
     }
   }
 
+  // *********** Function Clear Items **************** //
   const clearItem = () => {
-    setExpenses([])
-  }
-  
-  const handelDelete = id => {
-    console.log(`Item Deleted : ${id}`)
-    let tempExpenses = expenses.filter(item => item.id !== id)
-    setExpenses(tempExpenses)
-    handelAlert({type:"danger", text:"Item Deleted"})
-  }
+    setExpenses([]);
+  };
+
+  // *********** Function Delete Item **************** //
+  const handelDelete = (id) => {
+    console.log(`Item Deleted : ${id}`);
+    let tempExpenses = expenses.filter((item) => item.id !== id);
+    setExpenses(tempExpenses);
+    handelAlert({ type: "danger", text: "Item Deleted" });
+  };
+
+  // *********** Function Edit Item **************** //
   const handelEdit = (id) => {
-    let expense = expenses.find(item => item.id === id)
-    let {charge,amount} = expense;
-    setCharge(charge)
-    setAmount(amount)
-    setEdit(true)
-    setId(id)
-  }
+    let expense = expenses.find((item) => item.id === id);
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  };
 
-
-  return( <>
-    {alert.show && <Alert type={alert.type} text={alert.text}/>}
+  return (
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <h1>budget Calculator</h1>
       <main className="App">
-      <Forme charge={charge} amount={amount} handelAmount={handelAmount} handelCharge={handelCharge} handelSubmit={handelSubmit} edit={edit}/>
-    <List expenses={expenses} handelDelete={handelDelete} handelEdit={handelEdit} clearItem={clearItem}/> 
-
+        <Forme
+          charge={charge}
+          amount={amount}
+          handelAmount={handelAmount}
+          handelCharge={handelCharge}
+          handelSubmit={handelSubmit}
+          edit={edit}
+        />
+        <List
+          expenses={expenses}
+          handelDelete={handelDelete}
+          handelEdit={handelEdit}
+          clearItem={clearItem}
+        />
       </main>
-      <h1>Total Spending : $<span className="total">
-        {expenses.reduce((acc,curr) => {
-          return (acc += parseInt( curr.amount))
-        },0)}
-        </span></h1>
-  </>
-  )
+      <h1>
+        Total Spending : $
+        <span className="total">
+          {expenses.reduce((acc, curr) => {
+            return (acc += parseInt(curr.amount));
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,23 +1,31 @@
 import Item from "./ExpenseItem";
 
-export default function List({expenses, handelDelete, handelEdit, clearItem}) {
-    return (
-        <>
-        <ul className="list">
-        {
-            expenses.map((ex) => {
-                
-                return(
-                    <Item key={ex.id} expenses={ex} handelDelete={handelDelete} handelEdit={handelEdit}/>
-                )
-            })
-        }
-        {expenses.length > 0 && <button className="btn" onClick={clearItem}>
+export default function List({
+  expenses,
+  handelDelete,
+  handelEdit,
+  clearItem,
+}) {
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((ex) => {
+          return (
+            <Item
+              key={ex.id}
+              expenses={ex}
+              handelDelete={handelDelete}
+              handelEdit={handelEdit}
+            />
+          );
+        })}
+        {expenses.length > 0 && (
+          <button className="btn" onClick={clearItem}>
             Clear Expense
-
-         <i className="fa-solid fa-trash btn-icon"></i>
-            </button>}
-        </ul>
-        </>
-    )
+            <i className="fa-solid fa-trash btn-icon"></i>
+          </button>
+        )}
+      </ul>
+    </>
+  );
 }
